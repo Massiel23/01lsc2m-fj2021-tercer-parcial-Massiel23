1334815a220f4ffae36b77da4ffa4870b3724169
Massiel Lopez Lopez <20031307@uadeo.mx>
2021-05-25 13:12:54 -0600

    Primera Version

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _01lsc2m_fj2021_tercer_parcial_Massiel23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
